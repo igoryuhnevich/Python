7105b6b5f5064530fb923c429a1ae2f5c072f813
igor_yukhnevich <lp.adamz81@gmail.com>
2023-01-11 13:16:49 +0300

    create_file

--- App1/App_6_Sort.py
+++ App1/App_6_Sort.py
@@ -6,8 +6,24 @@ from collections import defaultdict , Counter
 
 arr = [random.randint(0,50) for i in range(10)]
 print(arr)
+
 my_regist=re.compile("[0-9]+",re.I)
-print(my_regist)
+
 lookup = defaultdict(int)
 my_counter = Counter()
-print(my_counter)
+
+def double(x) :
+ return x*2 
+
+def my_print(message="Hello, Python") :
+    print(message)
+my_print("Hi, igor")
+my_print(123)
+print(2)
+
+def subtract(a=0,b=0):
+    return a-b
+subtract(10,5) # 5
+subtract(0,5)  #-5
+subtract(b=5)  #-5
+
