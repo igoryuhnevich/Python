61c45e4f24ca9d8e6879fcbbe35738661b228957
igor_yukhnevich <lp.adamz81@gmail.com>
2023-01-11 16:18:52 +0300

    add

---
+++ App1/Corteg.py
@@ -0,0 +1,17 @@
+my_tuple= (1,2)
+other_tuple = 3,4 
+
+try:
+    my_tuple[1] =3
+except TypeError:
+    print("кортеж изменять нельзя")
+
+
+"""   Функуции и кортежи """
+
+def sum_and_product (x,y) :
+    return (x+y) , (x*y)
+sp = sum_and_product(2,3)    # =(5 , 6)
+s,p = sum_and_product(5,10)  # s = 15, p = 50
+
+
