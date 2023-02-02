ed14d66718f7987a84c1d7b3dc3d1d87eba0c043
igor_yukhnevich <lp.adamz81@gmail.com>
2023-02-02 12:10:11 +0300

    set()_{}

---
+++ App1/exemple/2.py
@@ -0,0 +1,11 @@
+some_dict = {'key_one': 'val_one', 'key_two': 'val_two'}
+some_set = set(some_dict)
+print(some_set)
+print(some_dict)
+""" glados = {['Great cake','hhhh','kkkk']}    error list 
+print(glados) """
+card_suit = ['heart','diamand','club','spade','spade']
+suit_set = set(card_suit)
+print(suit_set)
+print(card_suit)
+
