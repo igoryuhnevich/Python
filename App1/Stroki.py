dbb5d0f3644da5455e6007d397cf2c49cf3c94cd
igor_yukhnevich <lp.adamz81@gmail.com>
2023-02-09 15:57:44 +0300

    create_new

--- App1/Stroki.py
+++ App1/Stroki.py
@@ -1,12 +1,27 @@
-import sys
+import sys       # Python 3 - utf 8
+
+""" 
+1.Кодировка строк
+2.Максимальная длина строки в Python
+3.Пустая строка Python
+4.Перенос строк
+5.Конкатенация строк
+6.Сравнение строк
+7.Как удалить строку в Python
+8.Обращение по индексу
+9.Форматирование %, f-str, Template Str, format
+ """
+
 sys.maxsize
 print(sys.maxsize)   # 9223372036854775807
 
+# ------------------- Пустая строка
 ''
 ""
 str()
 
-text = 'one\ntwo\nthree'   # перенос по словам
+# ------------------- Перенос по словам
+text = 'one\ntwo\nthree'  
 
 # ------------------ Объединение
 s1 = "hello" + "world"
@@ -31,6 +46,59 @@ s1 = 'Intel'
 s2 = 'intel'
 s1 == s2     # false
 s1.lower() == s2.lower()  # true
+
+#------------------- Удаление строк
+s = 'test'
+s.replace('test','')    # ' '
+
+s = 'test'
+s = ''
+s                      # ' '
+
+# ----------------- Индекс 
+s ='abcdef'
+s[0]  # a
+s[1]  # b
+s[2]  # c
+s[-1] # f
+
+# --------------- Форматирование
+# %
+name = 'Alex'
+'Hello, %s' % name       # "Hello, Alex"
+# %d %s %c %r 
+'%d %s, %d %s' %(6,'bananas',10,'lemons')   # "6 bananas , 10 lemons"
+
+# str.format()
+'{}'.format(100)        # 100
+'{0},{1},{2}'.format('one','two','three')     # 'one, two, three'
+'{2},{1},{0}'.format('one','two','three')     # 'three, two, one'
+
+# f-строка    самая популярная
+name = 'Alex'
+f'Hello,{name}!'       # 'Hello,Alex!'
+
+a = 5
+b = 10
+f'Five pluse ten is {a+b} and not {2*(a+b)}'
+
+# Template Strings
+from string import Template
+name = 'Alex'
+age = 30
+s = Template('My name is $name. Im $age')
+print(s.substitute(name=name,age=age))
+
+
+
+ 
+
+
+
+
+
+
+
 
 
 
