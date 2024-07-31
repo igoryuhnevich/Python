c84b756424b2e23959acf74a218e01344af7bbfe
igor_yukhnevich <lp.adamz81@gmail.com>
2024-07-31 12:48:19 +0300

    new

---
+++ App1/dmain/hello.py
@@ -0,0 +1,16 @@
+
+a = '123' 
+
+def add():
+    pass
+
+
+ # print(globals())
+
+ # print( 'Imorted from:',__name__)
+def main():
+    pass
+
+
+if __name__ == '__main__' :
+    main()
--- App1/if_else_while.py
+++ App1/if_else_while.py
@@ -83,3 +83,4 @@ def all_divisors(number):
 print (all_divisors(6))
 
 
+
