a98e27e8211f7487103f2934fa1507e8b9317c70
igor_yukhnevich <lp.adamz81@gmail.com>
2024-08-02 11:41:47 +0300

    easy

--- App1/if_while_inner.py
+++ App1/if_while_inner.py
@@ -1,5 +1,27 @@
-d_lipt = input("введите числа с клавиатуры -  ") 
+#d_lipt = input("введите числа с клавиатуры -  ") 
 
-a = ( int(d) for d in d_lipt.split()) 
+#a = [ int(d) for d in d_lipt] 
 
-print(a)
+#print(a)
+# **************************************************
+"""
+a = [f'{i}*{j} = {i*j}'   
+ for i in range(3)         # таблица умножения
+ for j in range(4) ]
+
+print(a) """
+# ***************************************************
+'''
+matrix = [[0,1,2,3],
+          [10,11,12,13],
+          [20,21,22,23]]
+
+a = [ x
+        for row in matrix
+        for x in row
+        ]
+print(a) '''
+
+
+
+
