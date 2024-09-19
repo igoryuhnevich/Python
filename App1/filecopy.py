b7a4193cb7a96c9f28e41521e80517372b6ab4a0
igor_yukhnevich <lp.adamz81@gmail.com>
2024-09-19 11:23:41 +0300

    new_code

--- App1/filecopy.py
+++ App1/filecopy.py
@@ -1,8 +1,8 @@
 import os 
 import time
 
-
-sourse = ['C:\\usr','"C:\\VirtualBox VMs"']
+""" 
+sourse = ['C:\\usr']
 
 target_dir ='E:\\Backup'
 
@@ -10,20 +10,32 @@ target_poddir = target_dir + os.sep + time.strftime('%Y%m%d')
 
 name_arch = time.strftime('%H%M%S')
 
-comment = input("Input the comment -->")
-if len(comment) == 0:
-    target = target_poddir + os.sep + name_arch + '.rar'
-else : target = target_poddir + os.sep + '_'+ \
-    comment.replace(' ','_') + '.rar'
-
 if not os.path.exists(target_poddir):
     os.makedirs(target_poddir)
     print('Catalog is created',target_poddir)
 
-zip_command =  "WinRaR -qr {0} {1}".format(target,' '.join(sourse))
+target = target_poddir + os.sep + name_arch + '.ZIP'   
+
+zip_command =  '7zFM {0} . -i {1}'.format(target, ' '.join(sourse))
+
+print(zip_command)
 
 if os.system(zip_command) == 0:
     print('Suseful!',target)
 else:
-    print('Error')
+    print('Error') """
 
+
+ 
+source = ['E:\\teoriy','E:\\Users']
+target_dir = "E:\\DDD"  # Подставьте тот путь, который вы будете 
+#спользовать.
+target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
+zip_command = '7z a -r {0} {1}'.format(target, ' '.join(source))
+
+print(zip_command)
+if os.system(zip_command) == 0:
+    print('Резервная копия успешно создана в', target)
+else:
+    print('Создание резервной копии НЕ УДАЛОСЬ')
+    
